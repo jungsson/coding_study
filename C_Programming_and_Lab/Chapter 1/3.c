696d90ba467d8c0f13a691f51ef5a9b8f7be887a
이정현 <156187016+jungsson@users.noreply.github.com>
2024-08-20 23:22:38 +0900

    Create 3.c

---
+++ C_Programming_and_Lab/Chapter 1/3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+int main()
+{
+	int a;
+	scanf("%d", &a);
+	printf("%d%d%d%d%d%d\n",a,a,a,a,a,a);
+	printf("%d    %d\n",a,a);
+	printf("%d    %d\n",a,a);
+	printf("%d%d%d%d%d%d\n",a,a,a,a,a,a);
+	return 0;
+}
