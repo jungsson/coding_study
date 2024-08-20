46c9099520792c5cce136bf4d77f707363208adc
이정현 <156187016+jungsson@users.noreply.github.com>
2024-08-20 23:23:25 +0900

    Create 5.c

---
+++ C_Programming_and_Lab/Chapter 1/5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+int main()
+{
+	int a, b;
+	scanf("%d", &a);
+	scanf("%d", &b);
+	printf("My birthday is month %d date %d.",a,b);
+
+	return 0;
+}
